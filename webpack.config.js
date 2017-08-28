fb7a11578112b4d19244b52a760e4d2b46f9f3d5
Hanhan <HanhanTalk@hotmail.com>
2017-08-28 22:46:50 +0800

    fixed public path

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ var autoprefixer = require('autoprefixer')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist/'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: 'dist/',
     filename: 'build.js'
   },
   module: {
