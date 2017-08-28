ff5dcf3fa8cc3812abd170bf2d7706a946920c77
Hanhan <HanhanTalk@hotmail.com>
2017-08-28 23:36:30 +0800

    edit json path

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:3000';
-axios.defaults.baseURL = '/';
+// axios.defaults.baseURL = '.';
 axios.defaults.withCredentials = true;
 axios.defaults.crossDomain = true;
 
